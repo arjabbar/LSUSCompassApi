26f295610e9bc0e84a776b0edc3d929351b4ae34
Abdur Jabbar <arjabbar@yahoo.com>
2014-07-02 09:12:57 -0500

    Formated lecture loader.

--- lib/lecture_loader.rb
+++ lib/lecture_loader.rb
@@ -17,7 +17,9 @@ class LectureLoader
     page_collection.each do |search_result_page|
       search_result_parser = search_result_page.search_result
       lecture_parsers = search_result_parser.lecture_parsers
-      lecture_parsers.select(&:is_valid_lecture?).each do |lecture|
+      lecture_parsers
+      .select(&:is_valid_lecture?)
+      .each do |lecture|
         load_lecture lecture
       end
     end
@@ -26,20 +28,20 @@ class LectureLoader
   def load_lecture lecture_parser
     begin
       load_entity Lecture,
-        building: lecture_parser.building,
-        room: lecture_parser.room,
-        details_url: lecture_parser.details_link,
+        building:         lecture_parser.building,
+        room:             lecture_parser.room,
+        details_url:      lecture_parser.details_link,
         reference_number: lecture_parser.reference_number,
-        course: Course.find_or_create_by(code: lecture_parser.course_code),
-        campus: lecture_parser.campus,
-        session: lecture_parser.session,
-        start_date: lecture_parser.start_date,
-        end_date: lecture_parser.end_date,
-        start_time: lecture_parser.start_time,
-        end_time: lecture_parser.end_time,
-        seats_left: lecture_parser.seats_left,
-        professor: Professor.find_or_create_by(first_name: lecture_parser.professor)
-    rescue
+        course:           Course.find_or_create_by(code: lecture_parser.course_code),
+        campus:           lecture_parser.campus,
+        session:          lecture_parser.session,
+        start_date:       lecture_parser.start_date,
+        end_date:         lecture_parser.end_date,
+        start_time:       lecture_parser.start_time,
+        end_time:         lecture_parser.end_time,
+        seats_left:       lecture_parser.seats_left,
+        professor:        Professor.find_or_create_by(first_name: lecture_parser.professor)
+    rescue Exception => ex
       binding.pry
     end
   end
