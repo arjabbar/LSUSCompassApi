2fa7a53286928b667b9ea1200c8bc50b4f6f938f
Abdur Jabbar <arjabbar@yahoo.com>
2014-08-08 08:31:57 -0500

    Fixed test for adding reference number to lecture professors.

--- spec/models/lecture_professors_spec.rb
+++ spec/models/lecture_professors_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe LectureProfessors do
   describe "it's validations" do
     it { should validate_presence_of :professor_id }
-    it { should validate_presence_of :lecture_id }
+    it { should validate_presence_of :reference_number }
     it { should belong_to :lecture }
     it { should belong_to :professor }
   end
